2f37ced30eed344e0b7f1621848eaa2fff0ba637
Ellios <145565304+ellioslea@users.noreply.github.com>
2026-03-20 02:03:45 +0100

    Add files via upload
    version 1 app babe

---
+++ sw.js
@@ -0,0 +1,5 @@
+
+const CACHE='lc-v7';
+const ASSETS=['.'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
